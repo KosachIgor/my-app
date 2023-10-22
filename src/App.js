ddd166a8ead90f722c9917b659f554ebe4436813
Игорь Косач <105577694+KosachIgor@users.noreply.github.com>
2023-10-22 23:44:05 +1000

    app with JS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from "./logo.svg";
-import { MyComponent } from "./dateblock.js";
+import LOGO from "./logo.svg";
 import "./App.css";
 
-export function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React!
-                </a>
-                <MyComponent />
-            </header>
-        </div>
-    );
+export function Render() {
+    const container = document.createElement("div");
+    container.className = "App-header";
+
+    const logo = document.createElement("img");
+    logo.src = LOGO;
+    logo.className = "App-logo";
+    logo.alt = "logo";
+
+    const code = document.createElement("code");
+    code.textContent = "src/App.js";
+    code.className = "code";
+
+    const anchor = document.createElement("a");
+    anchor.src = "https://reactjs.org/";
+    anchor.textContent = "Learn REACT";
+    anchor.className = "App-link";
+
+    const text = document.createElement("p");
+    text.textContent = `Edit ${code.textContent} and save to reload.`;
+
+    const anchorBlock = document.createElement("div");
+
+    const dateBlock = document.createElement("div");
+    dateBlock.style.marginTop = "20px";
+    const currentDate = new Date();
+
+    dateBlock.append(currentDate);
+
+    anchorBlock.append(anchor);
+    container.append(logo, text, anchorBlock, dateBlock);
+    document.body.append(container);
+
+    return container;
 }
